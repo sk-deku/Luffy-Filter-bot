9cbca55faea5293cb835982465ea5b6553f23e1d
sk-deku <kesavansivakumar50@gmail.com>
2025-03-19 18:57:06 +0530

    Update token.py

--- database/token.py
+++ database/token.py
@@ -1,25 +1,33 @@
-from pymongo import MongoClient
+import pymongo
+import os
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+DB_NAME = "luffy_filter_bot"
 
 class TokenManager:
-    def __init__(self, mongo_uri):
-        self.client = MongoClient(mongo_uri)
-        self.db = self.client["AutoFilterBot"]
-        self.tokens = self.db["user_tokens"]
+    def __init__(self):
+        self.client = pymongo.MongoClient(MONGO_URI)
+        self.db = self.client[DB_NAME]
+        self.users = self.db["users"]
+
+    def add_user(self, user_id):
+        """Adds a new user to the database with 0 tokens if they don't exist."""
+        if not self.users.find_one({"user_id": user_id}):
+            self.users.insert_one({"user_id": user_id, "tokens": 0})
+
+    def update_tokens(self, user_id, amount):
+        """Updates user tokens by adding/subtracting a specified amount."""
+        self.users.update_one({"user_id": user_id}, {"$inc": {"tokens": amount}}, upsert=True)
 
     def get_tokens(self, user_id):
-        user = self.tokens.find_one({"user_id": user_id})
+        """Retrieves the number of tokens a user has."""
+        user = self.users.find_one({"user_id": user_id})
         return user["tokens"] if user else 0
 
-    def add_tokens(self, user_id, amount):
-        self.tokens.update_one({"user_id": user_id}, {"$inc": {"tokens": amount}}, upsert=True)
-
-    def deduct_token(self, user_id, amount=1):
-        user = self.tokens.find_one({"user_id": user_id})
-        if user and user.get("tokens", 0) >= amount:
-            self.tokens.update_one({"user_id": user_id}, {"$inc": {"tokens": -amount}})
+    def deduct_token(self, user_id):
+        """Deducts 1 token if the user has enough tokens."""
+        user = self.users.find_one({"user_id": user_id})
+        if user and user["tokens"] > 0:
+            self.update_tokens(user_id, -1)
             return True
         return False
-
-    def buy_tokens(self, user_id, amount):
-        # This should be integrated with a payment gateway
-        self.add_tokens(user_id, amount)
